fb84b157a23eeb304db53a0eab05beda67a4cd9e
Alain D'Ettorre <alain.det@gmail.com>
2021-06-21 08:12:41 +0200

    Add Dockerfile to MERN demo backend

--- apps/mern-demo/README.md
+++ apps/mern-demo/README.md
@@ -1,3 +1,15 @@
 # MERN stack demo
 
 ## Usage
+
+```
+docker network create mern-net
+
+docker run --rm -d --name mongodb-con --network mern-net mongo
+
+cd backend
+
+docker build -t mern-be-im .
+
+docker run -d --name mern-be-con -p 3001:80 -e APP_MONGODB_CONTAINER_NAME=mongodb-con -e APP_PORT=80 -e APP_NAME="Goals app" --network mern-net mern-be-im
+```
--- apps/mern-demo/backend/config/mongoose.js
+++ apps/mern-demo/backend/config/mongoose.js
@@ -1,9 +1,9 @@
 const containerName = process.env.APP_MONGODB_CONTAINER_NAME;
 
-// const dns = `mongodb://${containerName}:27017/course-goals`;
+const dns = `mongodb://${containerName}:27017/course-goals`;
 // const dns = `mongodb://172.17.0.2:27017/course-goals`;
 // const dns = `mongodb://host.docker.internal:27017/course-goals`;
-const dns = `mongodb://localhost:27017/course-goals`;
+// const dns = `mongodb://localhost:27017/course-goals`;
 
 const connectOptions = {
   useNewUrlParser: true,
